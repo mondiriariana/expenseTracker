43a01e5b7977c28a734a2dc3e51f03c520e69554
BenWordekemper <blw62889@creighton.edu>
2023-10-01 20:56:32 -0500

    Changes to login page and adding navbar

--- coin-wise/src/Login.js
+++ coin-wise/src/Login.js
@@ -1,12 +1,13 @@
 // Login.js
 import React, { Component } from 'react';
 import './Login.css'; // Import the CSS file
+import Navbar from './Navbar'; // Import the Navbar component
 
 class Login extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      username: '',
+      email: '',
       password: '',
       error: ''
     };
@@ -14,15 +15,15 @@ class Login extends Component {
 
   handleSubmit = (e) => {
     e.preventDefault();
-    const { username, password } = this.state;
+    const { email, password } = this.state;
 
     // Here, you can implement your login logic.
-    // For this example, we'll check if both fields are filled with a sample username and password.
-    if (username === 'sampleuser' && password === 'samplepassword') {
+    // For this example, we'll check if both fields are filled with a sample email and password.
+    if (email === 'sample@example.com' && password === 'samplepassword') {
       // Redirect to a success page or perform other actions here.
       alert('Login successful!');
     } else {
-      this.setState({ error: 'Invalid username or password' });
+      this.setState({ error: 'Invalid email or password' });
     }
   };
 
@@ -34,38 +35,45 @@ class Login extends Component {
   render() {
     return (
       <div className="container">
-         <div className="form-container"> 
-        <h2>Login</h2>
-        <form onSubmit={this.handleSubmit}>
-          <div className="form-group">
-            <div>
-              <label htmlFor="username">Username</label>
+        <Navbar />
+        <div className="container">
+          <div className="form-container">
+            <h2>Login</h2>
+            <form onSubmit={this.handleSubmit}>
+              <div className="form-group">
+                <div>
+                  <label htmlFor="email">Email</label>
+                </div>
+                <input
+                  type="email"
+                  id="email"
+                  name="email"
+                  value={this.state.email}
+                  onChange={this.handleInputChange}
+                  required
+                />
+              </div>
+              <div className="form-group">
+                <div>
+                  <label htmlFor="password">Password</label>
+                </div>
+                <input
+                  type="password"
+                  id="password"
+                  name="password"
+                  value={this.state.password}
+                  onChange={this.handleInputChange}
+                  required
+                />
+              </div>
+              <button type="submit">Login</button>
+            </form>
+            {this.state.error && <p className="error-message">{this.state.error}</p>}
+            <div className="additional-links">
+              <a href="#forgot-password"> Forgot Password </a>
+              <a href="#sign-up">Sign up</a>
             </div>
-            <input
-              type="text"
-              id="username"
-              name="username"
-              value={this.state.username}
-              onChange={this.handleInputChange}
-              required
-            />
           </div>
-          <div className="form-group">
-            <div>
-              <label htmlFor="password">Password</label>
-            </div>
-            <input
-              type="password"
-              id="password"
-              name="password"
-              value={this.state.password}
-              onChange={this.handleInputChange}
-              required
-            />
-          </div>
-          <button type="submit">Login</button>
-        </form>
-        {this.state.error && <p className="error-message">{this.state.error}</p>}
         </div>
       </div>
     );
